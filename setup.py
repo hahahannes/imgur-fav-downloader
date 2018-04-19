2d292a5d723786206409b61e8ff131077f06589b
Hannes <mail@hannesh.de>
2018-04-19 21:01:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 setup(name='imgur_fav_downloader',
       version='1.0',
       description='Imgur Favorites Downloader',
-      author='Hansi96',
       license='MIT',
       install_requires=[
           'requests',
